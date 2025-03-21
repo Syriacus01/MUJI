15cfff561da97057103d35039add2878e8e401e0
TaehanYoon <yunk222@naver.com>
2025-03-21 09:41:35 +0900

    [#3]Feat : 장소 location para 추가

--- MUJI/MUJI/MusicRecommendation/FetchWeather.swift
+++ MUJI/MUJI/MusicRecommendation/FetchWeather.swift
@@ -5,43 +5,54 @@
 //  Created by 윤태한 on 3/19/25.
 //
 
-/*
+/* ===================== 사용 예시 =====================
 FetchWeather().fetchWeather(lat: "37.56", lon: "126.97") { weatherInfo in
-    print(weatherInfo)
     Task {
-        print("ChatGPT에 요청을 시작합니다.")
-        let songs = await SearchChatGPT().search(input: weatherInfo)
+        let songs = await SearchChatGPT().search(location: "서울시 관악구 남부순환로", weather: weatherInfo, emotion: "행복함", age: 26, genre: "힙합")
         
         if !songs.isEmpty {
-            print("=========== 추천 노래 리스트 ===========\n\(songs)")
-            print("====================================")
+            print("============== 추천 노래 리스트 ==============\n\(songs)")
+            print("==========================================")
+            print("노래 추천 완료.")
         } else {
             print("ChatGPT로부터 추천 노래를 받지 못했습니다.")
         }
     }
-}*/
+}
+ ========================= 사용 예시 =====================*/
 
 import Foundation
 
 class FetchWeather {
     
-    let weatherAPIKey = ""
+    let weatherAPIKey: String = {
+        guard let key = ProcessInfo.processInfo.environment["WEATHER_API_KEY"] else {
+            fatalError("API_KEY가 없음.")
+        }
+        return key
+    }()
+    let weatherURL: String = {
+        guard let key = ProcessInfo.processInfo.environment["WEATHER_URL"] else {
+            fatalError("API_KEY가 없음.")
+        }
+        return key
+    }()
     
-    // MARK: - 날씨 API 호출 함수 (위도와 경도를 문자열 매개변수로 받음)
+    
     func fetchWeather(lat: String, lon: String, completion: @escaping (String) -> Void) {
-        let weatherURLString = "/data/3.0/onecall?lat=\(lat)&lon=\(lon)&lang=kr&appid=\(weatherAPIKey)"
+        
+        let weatherURLString = weatherURL + "\(lat)&lon=\(lon)&lang=kr&appid=\(weatherAPIKey)"
         
         guard let weatherURL = URL(string: weatherURLString) else {
-            print("잘못된 URL입니다.")
+            print("잘못된 URL")
             return
         }
         
         URLSession.shared.dataTask(with: weatherURL) { data, response, error in
             if let error = error {
-                print("에러 발생: \(error)")
+                print("에러발생: \(error)")
                 return
             }
-            
             guard let data = data else {
                 print("데이터가 없습니다.")
                 return
@@ -51,17 +62,16 @@ class FetchWeather {
                 let weatherResponse = try JSONDecoder().decode(WeatherResponse.self, from: data)
                 let current = weatherResponse.current
                 
-                // 체감 온도 변환: 켈빈(K)을 섭씨(℃)로 변환
                 let feelsLikeCelsius = current.feels_like - 273.15
                 let description = current.weather.first?.description ?? "날씨 정보 없음"
                 
-                // ChatGPT에게 노래 추천 요청
-                let weatherInfo = "현재 날씨는 '\(description)', 체감 온도는 \(String(format: "%.2f", feelsLikeCelsius))℃."
+                let weatherInfo = "현재 날씨: \(description), 체감 온도: \(String(format: "%.2f", feelsLikeCelsius))℃"
                 completion(weatherInfo)
                 
             } catch {
                 print("디코딩 에러: \(error)")
             }
+            
         }.resume()
     }
 }
--- MUJI/MUJI/MusicRecommendation/SearchChatGPT.swift
+++ MUJI/MUJI/MusicRecommendation/SearchChatGPT.swift
@@ -7,17 +7,26 @@
 
 import Dispatch
 import ChatGPTSwift
+import Foundation
 
 class SearchChatGPT {
     
-    let chatGPTAPIKey = ""
+    let chatGPTAPIKey: String = {
+        guard let key = ProcessInfo.processInfo.environment["CHATGPT_API_KEY"] else {
+            fatalError("API_KEY가 없음.")
+        }
+        return key
+    }()
     
-    func search(input: String) async -> String {
-        let formalString = " 현재 날씨에 어울리는 노래 3개 추천해줘(다른 글씨는 빼고 가수 - 노래명 형식으로 출력해줘)"
+    func search(location: String, weather: String, emotion: String, age: Int, genre: String) async -> String {
+        let para = "현위치는 \(location), \(weather), 감정 상태: \(emotion), 나이: \(age)세, 선호 장르: \(genre)"
+        let formalString = "이 조건들과 알맞는 노래 3개 추천해줘 (다른 글씨는 빼고 가수 - 노래명 형식으로 출력해줘)"
+        
         let api = ChatGPTAPI(apiKey: chatGPTAPIKey)
         
+        print("\(para)\n\n위의 조건들로 ChatGPT에 노래 추천 요청을 시작합니다.\n")
         do {
-            let response = try await api.sendMessage(text: input + formalString)
+            let response = try await api.sendMessage(text: para + formalString)
             return response
         } catch {
             print("에러 발생: \(error)")
@@ -25,3 +34,4 @@ class SearchChatGPT {
         }
     }
 }
+
